79a29b27339e64a02c860823039f0ab12a251a15
LunaAmora <luna.mberry@gmail.com>
2023-12-20 11:22:38 -0400

    Fix query typo

--- src/system.rs
+++ src/system.rs
@@ -5,42 +5,42 @@ pub trait System {
     fn call_with_data(self: Box<Self>, data: &mut EntityData);
 }
 
-pub fn new<C: 'static>(querry: impl Querry<C> + 'static) -> Box<dyn System> {
-    Box::new(QuerryHandler {
-        querry,
-        callable: Querry::call,
+pub fn new<C: 'static>(query: impl Query<C> + 'static) -> Box<dyn System> {
+    Box::new(QueryHandler {
+        query,
+        callable: Query::call,
     })
 }
 
-struct QuerryHandler<Q> {
-    pub querry: Q,
+struct QueryHandler<Q> {
+    pub query: Q,
     pub callable: fn(Q, &EntityData),
 }
 
-impl<Q: Clone> Clone for QuerryHandler<Q> {
+impl<Q: Clone> Clone for QueryHandler<Q> {
     fn clone(&self) -> Self {
         Self {
-            querry: self.querry.clone(),
+            query: self.query.clone(),
             callable: self.callable,
         }
     }
 }
 
-impl<Q: Clone> System for QuerryHandler<Q> {
+impl<Q: Clone> System for QueryHandler<Q> {
     fn clone_box<'a>(&'a self) -> Box<dyn System + 'a> {
         Box::new(self.clone())
     }
 
     fn call_with_data(self: Box<Self>, data: &mut EntityData) {
-        (self.callable)(self.querry, data);
+        (self.callable)(self.query, data);
     }
 }
 
-pub trait Querry<C>: Clone {
+pub trait Query<C>: Clone {
     fn call(self, data: &EntityData);
 }
 
-impl<F, R> Querry<R> for F
+impl<F, R> Query<R> for F
 where
     F: for<'f> Fn(R::Ref<'f>) + Clone,
     R: RefType<Type: Component>,
@@ -52,7 +52,7 @@ where
     }
 }
 
-impl<F, R1, R2> Querry<(R1, R2)> for F
+impl<F, R1, R2> Query<(R1, R2)> for F
 where
     F: for<'f1, 'f2> Fn(R1::Ref<'f1>, R2::Ref<'f2>) + Clone,
     R1: RefType<Type: Component>,
